d073e032404f98fd586c6a524b41600948e4c44f
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-09-20 16:20:44 +0530

    Add files via upload

---
+++ cons.java
@@ -0,0 +1,30 @@
+class student
+{
+    student()
+    {
+      System.out.println("student name is Suraj");
+    }
+}
+class city
+{
+    city()
+    {
+        System.out.print("student city is Ooty");
+    }
+}
+class age
+{
+    age()
+    {
+        System.out.println("student age is 24");
+    }
+}
+class cons
+{
+    public static void main(String args[])
+    {
+        student obj=new student();
+        city obj1=new city();
+        age obj2=new age();
+    }
+}
