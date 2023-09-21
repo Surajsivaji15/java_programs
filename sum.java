7121c819facc8e5222a3a73c527efc8ac9f41065
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-09-21 22:11:50 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,17 @@
+class values
+ {
+     int getdata(int x,int y)
+	{
+  	   return x+y;
+    
+	}
+ }
+
+
+class sum
+ {
+    public static void main(String asd[])
+	{
+	  System.out.println(new values().getdata(100,120));	  
+	}
+ }
