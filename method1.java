d8c016514fb6c69c286c4cedd82c2b33da3363f4
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-09-22 16:50:54 +0530

    Add files via upload

---
+++ method1.java
@@ -0,0 +1,15 @@
+class details
+{
+    public void getdetails()
+    {
+        System.out.println("MY NAME IS SURAJ");
+        System.out.println("MY CITY IS OOTY");
+    }
+}
+class method1
+{
+    public static void main(String args[])
+    {
+    new details().getdetails();
+    }
+}
