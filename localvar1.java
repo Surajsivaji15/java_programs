8cfb75cbe702be40fa9f0eeffbdd64ba22519232
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-09-25 11:46:18 +0530

    Add files via upload

---
+++ localvar1.java
@@ -0,0 +1,16 @@
+class details
+  {
+    details(String name,int age,String city)
+    {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(city);
+    }
+  }
+class localvar1
+{
+    public static void main(String args[])
+    {
+        details obj=new details("Mohan",21,"salem");
+    }
+}
