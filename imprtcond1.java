5fd783a661d9fc8e8157efc550de081c3fa11daf
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-09-30 15:55:36 +0530

    Add files via upload

---
+++ imprtcond1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class sample
+{
+    Scanner s=new Scanner(System.in);
+    sample()
+    {
+        System.out.println("Enter the name1= ");
+        String x=s.nextLine();
+        System.out.println("Enter the name2= ");
+        String y=s.nextLine();
+
+        if(x==x)
+         {
+            System.out.println("Your name is matched");
+         }
+        else
+         {
+            System.out.println("Your name is unmatched");
+         }
+        
+    }
+}
+class imprtcond1
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
