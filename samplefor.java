65eb198e0111df1961388e2ffb67b345d02fb1bd
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-03 13:00:52 +0530

    Add files via upload

---
+++ samplefor.java
@@ -0,0 +1,18 @@
+class sample
+{
+    sample()
+     {
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("welcome");
+        }
+
+     }
+}
+class samplefor
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
