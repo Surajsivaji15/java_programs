0881c296c89d805e591488796574c9adcd3fb91e
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-04 13:51:13 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,24 @@
+class sample 
+{
+    sample()
+    {
+        int x[]=new int[4];
+        x[0]=11;
+        x[1]=22;
+        x[2]=33;
+        x[3]=44;
+
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+        System.out.println(x[3]);
+
+    }
+}
+
+public class array1 {
+    public static void main(String sd[])
+    {
+         sample s=new sample();
+    }
+}
