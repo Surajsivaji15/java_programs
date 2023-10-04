9438badb6b4cabb1e0020adeaa471009f7277add
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-04 14:11:05 +0530

    Add files via upload

---
+++ array2.java
@@ -0,0 +1,23 @@
+class sample
+{
+    sample()
+    {
+        String cars[]=new String[4];
+        cars[0]="thar";
+        cars[1]="Scorpio";
+        cars[2]="bolero";
+        cars[3]="xuv500";
+
+        System.out.println(cars[0]);
+         System.out.println(cars[1]);
+          System.out.println(cars[2]);
+           System.out.println(cars[3]);
+    }
+}
+class array2
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
