e62278f8dcbd3b6727cb1d004dc904ac90acac02
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-06 22:57:56 +0530

    Add files via upload

---
+++ replaceprgm.java
@@ -0,0 +1,16 @@
+class sample
+{
+    sample()
+    {
+      String s="my name is suraj";
+      String replaceString=s.replace("suraj", "surya");
+      System.out.println(replaceString);
+    }
+}
+class replaceprgm
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
