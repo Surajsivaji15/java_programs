c12b4aa832bd3b435459fc95847b856a36319ace
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-06 12:10:31 +0530

    Add files via upload

---
+++ sbappend.java
@@ -0,0 +1,27 @@
+/* 
+class StringBufferExample
+{  
+public static void main(String args[])
+{  
+StringBuffer sb=new StringBuffer("Hello ");  
+sb.append("Java");//now original string is changed  
+System.out.println(sb);//prints Hello Java  
+}  
+}  */
+class sample
+{
+    sample()
+    {
+        StringBuffer sb=new StringBuffer("PRAVEEN");
+        sb.append(" ARUMUGAM");
+        System.out.println(sb);
+
+    }
+}
+class sbappend
+{
+    public static void main(String args[])
+    {
+       new sample();
+    }
+}
