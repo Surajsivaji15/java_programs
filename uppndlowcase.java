f4edb7c8ed65f7e9b1ba5c3c33b4d26659ee428e
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-06 14:52:28 +0530

    Add files via upload

---
+++ uppndlowcase.java
@@ -0,0 +1,12 @@
+
+
+class uppndlowcase
+{ 
+    
+    public static void main(String args[])
+    {
+        String s="suraj";
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+    }
+}
