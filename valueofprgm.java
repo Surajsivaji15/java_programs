4f122456d3a075306dad910e67764deb3e235e54
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-06 22:24:50 +0530

    Add files via upload

---
+++ valueofprgm.java
@@ -0,0 +1,17 @@
+class sample
+{
+    sample()
+    {
+        int a=20;
+        String s=String.valueOf(a);
+        System.out.println(s+23);
+
+    }
+}
+class valueofprgm
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
