b1b8ec5a1cb9e86e92c68d67fc3d504a733c3581
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-06 14:59:01 +0530

    Add files via upload

---
+++ trimprgm.java
@@ -0,0 +1,9 @@
+class trimprgm
+{
+    public static void main(String args[])
+    {
+        String s="Praveen kumar";
+        System.out.println(s);
+        System.out.println(s.trim());
+    }
+}
