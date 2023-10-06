d10232addab4bfe20240f5ccd64d0693d4853c63
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-06 13:13:38 +0530

    Add files via upload

---
+++ sbreverse.java
@@ -0,0 +1,17 @@
+class sample
+{
+    sample()
+    {
+      StringBuffer sb=new StringBuffer("PRAVEEN");
+      sb.reverse();
+      System.out.println(sb);
+
+    }
+}
+class sbreverse
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
