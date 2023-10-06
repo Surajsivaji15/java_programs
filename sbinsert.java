34ad81a70550e7572902fbb9623f4a73f7f4efd3
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-06 12:40:52 +0530

    Add files via upload

---
+++ sbinsert.java
@@ -0,0 +1,17 @@
+class sample
+{
+    sample()
+    {
+      StringBuffer sb=new StringBuffer("PAVEEN");
+      sb.insert(1,"R");
+      System.out.println(sb);
+
+    }
+}
+class sbinsert
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
