4127dd8abc1fa2a3780975a97306930daadca642
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-06 15:05:26 +0530

    Add files via upload

---
+++ strtndendprgm.java
@@ -0,0 +1,9 @@
+class strtndendprgm
+{
+    public static void main(String args[])
+    {
+        String s="Praveen";
+        System.out.println(s.startsWith("P"));
+        System.out.println(s.endsWith("n"));
+    }
+}
