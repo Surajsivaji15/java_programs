fe9c45999a67de0ac30b1feb1fc05aa648fae781
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-06 13:10:34 +0530

    Add files via upload

---
+++ sbdelete.java
@@ -0,0 +1,17 @@
+class sample
+{
+    sample()
+    {
+      StringBuffer sb=new StringBuffer("PRAVEEN");
+      sb.delete(1,4);
+      System.out.println(sb);
+
+    }
+}
+class sbdelete
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
