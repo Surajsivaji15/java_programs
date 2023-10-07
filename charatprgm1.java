3ddb8020cec93a106f6c76f6492f71002bf767a0
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-07 11:00:31 +0530

    Add files via upload

---
+++ charatprgm1.java
@@ -0,0 +1,16 @@
+class sample
+{
+    sample()
+    {
+       String name="Praveen";
+       char ch=name.charAt(4);
+       System.out.println(ch);
+    }
+}
+class charatprgm1
+{
+    public static void main(String args[])
+    {
+        sample s=new sample();
+    }
+}
