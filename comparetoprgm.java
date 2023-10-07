d8dfa88f6f31bbf4f49de607d7550f4206812ed2
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-07 11:13:33 +0530

    Add files via upload

---
+++ comparetoprgm.java
@@ -0,0 +1,20 @@
+class sample
+{
+    sample()
+    {
+       String s1="suraj";
+       String s2="praveen";
+       String s3="hari";
+       String s4="dinesh";
+       System.out.println(s1.compareTo(s2));
+       System.out.println(s1.compareTo(s3));
+       System.out.println(s1.compareTo(s4));
+    }
+}
+class comparetoprgm
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
