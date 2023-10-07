e163a7d9ddbbb9854bc32fb082c7c95b99243afd
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-07 11:26:29 +0530

    Add files via upload

---
+++ concatprgm.java
@@ -0,0 +1,17 @@
+class sample
+{
+    sample()
+    {
+        String s1="Praveen";
+        String s2=" Arumugam";
+        System.out.println(s1.concat(s2));
+
+    }
+}
+class concatprgm
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
