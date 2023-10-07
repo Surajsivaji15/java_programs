a133cd5a9902fffa9e1557d8f11c0a20b2f53597
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-07 12:25:46 +0530

    Add files via upload

---
+++ replaceprgm1.java
@@ -0,0 +1,17 @@
+class sample
+{
+    sample()
+    {
+        String msg="good morning to everyone";
+        String msg1=msg.replace("morning","afternoon");
+        System.out.println(msg1);
+
+    }
+}
+class replaceprgm1
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
