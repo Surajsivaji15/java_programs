a025a7fc2758d7ab60261dca5301631e9a5f9d5c
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-07 11:57:50 +0530

    Add files via upload

---
+++ equalsignorecase.java
@@ -0,0 +1,20 @@
+class sample
+{
+    sample()
+    {
+       String s1="suraj";
+       String s2="suraj";
+       String s3="hari";
+       String s4="SURAJ";
+       System.out.println(s1.equalsIgnoreCase(s2));
+       System.out.println(s1.equalsIgnoreCase(s3));
+       System.out.println(s1.equalsIgnoreCase(s4));
+    }
+}
+class equalsignorecase
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
