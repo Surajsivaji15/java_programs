4e36700042990b597798bf14e5c52a329b46e3df
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-07 11:38:35 +0530

    Add files via upload

---
+++ containsprgm.java
@@ -0,0 +1,16 @@
+class sample
+{
+    sample()
+    {
+       String msg="good morning to everyone";
+       System.out.println(msg.contains("good morning"));
+       System.out.println(msg.contains("evening"));
+    }
+}
+class containsprgm
+{
+    public static void main(String args[])
+    {
+        new sample();
+    }
+}
