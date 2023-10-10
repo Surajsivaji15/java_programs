59d73be9f99386f9016c9d5764ffe72556644a30
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-10 13:28:11 +0530

    Add files via upload

---
+++ arraylist1.java
@@ -0,0 +1,23 @@
+import java.util.*;  
+  
+  
+class cars
+{
+    ArrayList a=new ArrayList();
+    cars()
+    {
+        a.add("alto");
+        a.add("thar");
+        a.add("i10");
+        a.add("i20");
+        System.out.println(a);
+    }
+
+}
+class arraylist1
+{
+    public static void main(String args[])
+    {
+        new cars();
+    }
+}
