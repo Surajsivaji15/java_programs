93cdc6bbcc017ff8365890c99d79a499ee2df9ca
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-10 16:10:51 +0530

    Add files via upload

---
+++ arraylist3.java
@@ -0,0 +1,26 @@
+class names
+ {
+	ArrayList<String> a=new ArrayList<String>();
+    
+	names()
+	{
+		a.add("suraj");
+		a.add("surya");
+		a.add("ram");
+		a.add("raj");
+		
+		for(String name:a)
+		{
+			System.out.println(name);
+		}
+
+
+	}
+ }
+class arraylist3
+{
+	public static void main(String args[])
+	{
+		new names();
+	}
+}
