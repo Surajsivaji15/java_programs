0a298928444aca5e673191257d479954d35e95d5
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-11 12:22:51 +0530

    Add files via upload

---
+++ formatprgm.java
@@ -0,0 +1,19 @@
+class details
+{
+    details()
+    {
+        String name="Surya";
+        String s1=String.format("name is %s",name);
+        String s2=String.format("number is %f",33.34);
+        System.out.println(s1);
+        System.out.println(s2);
+
+    }
+}
+class formatprgm
+{
+    public static void main(String args[])
+    {
+        new details();
+    }
+}
