5fa35e059c65284d423d1a16b302ddb2180712b8
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-11 14:16:19 +0530

    Add files via upload

---
+++ joinprgm.java
@@ -0,0 +1,16 @@
+class dob
+{
+    dob()
+    {
+       String s1=String.join("/","15","11","1998");
+       System.out.println(s1);
+
+    }
+}
+class joinprgm
+{
+    public static void main(String args[])
+    {
+        new dob();
+    }
+}
