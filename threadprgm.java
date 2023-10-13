5a90e034c51fcd412e4812492d9632aedf2802d4
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-13 12:31:08 +0530

    Add files via upload

---
+++ threadprgm.java
@@ -0,0 +1,26 @@
+class sample extends Thread
+{
+    Thread t=new Thread(this);
+    sample()
+    {
+        for(int i=0;i<10;i++)
+        {
+            try{
+                System.out.println(i);
+                t.sleep(2000);
+            }catch(Exception d){}
+        }
+      
+    }
+}
+
+
+
+class threadprgm
+{
+    public static void main(String asd[])
+    {
+        sample obj=new sample();
+        // System.out.println("Main method"+Thread.getName());
+    }
+}
