da61f2eea479d5ffad35d3eb7a308641aa16e5ee
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-13 15:13:30 +0530

    Add files via upload

---
+++ mathprgm.java
@@ -0,0 +1,18 @@
+class math1
+{
+    math1()
+    {
+        int x=22;
+        int y=11;
+        System.out.println("The absolute value is "+Math.abs(x));
+        System.out.println("The absolute value is "+Math.abs(y));
+
+    }
+}
+class mathprgm
+{
+    public static void main(String args[])
+    {
+        new math1();
+    }
+}
