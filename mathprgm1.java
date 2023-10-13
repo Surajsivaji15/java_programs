c8981e38fb64806eab26c2118ed9e1c164655c86
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-13 15:21:13 +0530

    Add files via upload

---
+++ mathprgm1.java
@@ -0,0 +1,16 @@
+class math1
+{
+    math1()
+    {
+        int x=22;
+        int y=33;
+        System.out.println("the maximum value of x and y is "+Math.max(x,y));
+    }
+}
+class mathprgm1
+{
+    public static void main(String args[])
+    {
+        new math1();
+    }
+}
