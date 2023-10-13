5e5e8764f4652f40ce8a12ffb3bdcf7596f51f88
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-13 15:50:01 +0530

    Add files via upload

---
+++ mathprgm5.java
@@ -0,0 +1,16 @@
+class math5
+{
+    math5()
+    {
+        int x=777;
+        System.out.println("the cuberoot of given nmbr is "+Math.cbrt(x));
+
+    }
+}
+class mathprgm5
+{
+    public static void main(String args[])
+    {
+    new math5();
+    }
+}
