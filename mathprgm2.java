13bf6104988f6deaabc5cccffcc9f2363231bf25
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-13 15:25:46 +0530

    Add files via upload

---
+++ mathprgm2.java
@@ -0,0 +1,17 @@
+class math2
+{
+    math2()
+    {
+        int x=22;
+        int y=34;
+        System.out.println("the minimum value is "+Math.min(x,y));
+
+    }
+}
+class mathprgm2
+{
+    public static void main(String args[])
+    {
+        new math2();
+    }
+}
