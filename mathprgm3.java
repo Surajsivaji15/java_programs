1a81bfe205cf8a3f3e88c9b94aa03ea1c52f2a40
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-13 15:33:19 +0530

    Add files via upload

---
+++ mathprgm3.java
@@ -0,0 +1,16 @@
+class math3
+{
+    math3()
+    {
+        Float x=33.44f;
+        System.out.println("the round of the number is "+Math.round(x));
+
+    }
+}
+class mathprgm3
+{
+    public static void main(String aegs[])
+    {
+        new math3();
+    }
+}
