2fab18cdb60576c507313822b6fd230ed5feaddc
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-13 15:40:01 +0530

    Add files via upload

---
+++ mathprgm4.java
@@ -0,0 +1,15 @@
+class math4
+{
+    math4()
+    {
+       int x=100;
+       System.out.println("the square root of given value is "+Math.sqrt(x));
+    }
+}
+class mathprgm4
+{
+    public static void main(String args[])
+    {
+        new math4();
+    }
+}
