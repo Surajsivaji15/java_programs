c43af9ef002791eff91abf24a79c54877b646646
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-14 11:24:59 +0530

    Add files via upload

---
+++ mathprgm7.java
@@ -0,0 +1,18 @@
+class math7
+{
+    math7()
+    {
+        double a=20;
+        System.out.println("Sin value of a is "+Math.sin(a));
+        System.out.println("Cos value of a is "+Math.cos(a));
+        System.out.println("Tan value of a is "+Math.tan(a));
+
+    }
+}
+class mathprgm7
+{
+    public static void main(String args[])
+    {
+        new math7();
+    }
+}
