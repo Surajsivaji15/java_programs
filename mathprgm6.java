136dd3a2ba4afe489879320546a3a322487d63a2
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-14 11:07:15 +0530

    Add files via upload

---
+++ mathprgm6.java
@@ -0,0 +1,17 @@
+class math6
+{
+    math6()
+    {
+        int x=5;
+        int y=2;
+        System.out.println("The power of x and y is "+Math.pow(x,y));
+
+    }
+}
+class mathprgm6
+{
+    public static void main(String args[])
+    {
+        new math6();
+    }
+}
