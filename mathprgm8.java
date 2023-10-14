0043eb238e899b2b54ca197bca2de6f51f59c4b4
Surajsivaji15 <145020972+Surajsivaji15@users.noreply.github.com>
2023-10-14 11:37:04 +0530

    Add files via upload

---
+++ mathprgm8.java
@@ -0,0 +1,18 @@
+class math8
+{
+    math8()
+    {
+        double a=44;
+        double b=22;
+        System.out.println("The ceil value of given value is "+Math.ceil(a));
+        System.out.println("The sign of given value is "+Math.signum(b));
+
+    }
+}
+class mathprgm8
+{
+    public static void main(String args[])
+    {
+        new math8();
+    }
+}
